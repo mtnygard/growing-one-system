a51bbea40fcdcaf4c544dfc894d27504da946ee4
Michael Nygard <mtnygard@gmail.com>
2019-03-22 15:45:47 -0500

    The front end should be built with shadow-cljs

---
+++ front-end/.dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((cider-default-cljs-repl . 'shadow)
+         (cider-shadow-cljs-parameters . "watch")
+         (cider-check-cljs-repl-requirements . nil))))
---
+++ front-end/src/app/lib.cljs
@@ -0,0 +1,7 @@
+(ns app.lib)
+
+(println "[lib] loading")
+
+(def c 99)
+
+(defonce d 2)
