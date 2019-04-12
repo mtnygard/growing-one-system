39d62a9382481f850dacdc471860b008b9237d89
Michael Nygard <mtnygard@gmail.com>
2019-04-12 17:34:29 -0500

    Only use devtools via preloads

--- ui/src/igles/main.cljs
+++ ui/src/igles/main.cljs
@@ -1,15 +1,12 @@
 (ns igles.main
   (:require [igles.lib :as lib]
             [re-frame.core :as rf]
-            [devtools.core :as devtools]
             [reagent.core :as reagent]
             [igles.views :as views]
             [igles.events]
             [igles.subs]))
 
 ;; Debugging aids
-
-(devtools/install!)
 (enable-console-print!)
 
 (defn mount-root []
