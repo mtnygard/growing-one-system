2c8e374df9510e37a2ad356643e20db09a0497cb
Michael Nygard <mtnygard@gmail.com>
2019-08-11 18:33:22 -0500

    Use the first string returned from a query as the template.

--- cli/src/gos/mustache.clj
+++ cli/src/gos/mustache.clj
@@ -6,7 +6,19 @@
 (s/def ::mustache-schema (s/schema [::mustache]))
 (s/def ::mustache-result (s/select ::mustache-schema [:mustache]))
 
+(s/def ::query-result set?)
+(s/def ::query-fields sequential?)
+(s/def ::query-schema (s/schema [::query-result ::query-fields]))
+(s/def ::template-in-query (s/select ::query-schema [:query-result {::query-result set?}]))
+
+(defn template-in-query? [m]
+  (s/valid? ::template-in-query m))
+
+(defn extract-template [m]
+  (-> m :query-result ffirst))
+
 (sprint/use ::mustache-result
   (fn [{:keys [mustache] :as data}]
-    (let [data (dissoc data :mustache)]
+    (let [data (dissoc data :mustache)
+          mustache (if (template-in-query? mustache) (extract-template mustache) mustache)]
       (print (render mustache data)))))
