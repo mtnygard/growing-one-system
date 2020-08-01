65326b0cb88cf66142a0cf1d77413c2c45d019ff
Michael Nygard <mtnygard@gmail.com>
2020-08-01 19:30:28 +0000

    Use hygenic macro

--- api/test/gos/world_test.clj
+++ api/test/gos/world_test.clj
@@ -1,12 +1,10 @@
 (ns gos.world-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.test :refer [are deftest is testing]]
             [gos.ast :as ast]
             [gos.datomic-fixtures :as fix]
             [gos.db :as db]
             [gos.exec :as exec]
-            [gos.parser :as parser]
-            [gos.problems :refer [problems?]]
-            [gos.debug :as debug]))
+            [gos.problems :refer [problems?]]))
 
 (defn- process
   [state]
@@ -14,8 +12,6 @@
 
 (defn- start-state []
   (exec/initial-state (fix/adapter)))
-
-(def ^:private attr? fix/lookup-attribute)
 
 (defn attr? [k exp]
   (is (= exp (select-keys (fix/lookup-attribute k) (keys exp)))))
@@ -31,93 +27,93 @@
     (ast/evaluate  {:dbadapter (fix/adapter)})
     :query-result))
 
-(defmacro after [strs & assertions]
+(defmacro after [[sym strs] & assertions]
   `(fix/with-database []
-     (let [~'end-state (reduce #(exec/process (exec/with-input %1 %2)) (start-state) ~strs)]
+     (let [~sym (reduce #(exec/process (exec/with-input %1 %2)) (start-state) ~strs)]
        ~@assertions)))
 
 (deftest attribute
   (testing "can be added"
     (testing "as single-valued"
-      (after ["attr name string one;"]
+      (after [end-state ["attr name string one;"]] 
         (is (not (problems? end-state)))
         (attr? :name {:db/ident       :name
                       :db/valueType   :db.type/string
                       :db/cardinality :db.cardinality/one})))
 
     (testing "can be multi-valued"
-      (after ["attr scores long many;"]
+      (after [end-state ["attr scores long many;"]]
         (is (not (problems? end-state)))
         (attr? :scores {:db/ident       :scores
                         :db/valueType   :db.type/long
                         :db/cardinality :db.cardinality/many}))))
 
   (testing "can be expanded"
-    (after ["attr name string one;"
-            "attr name string many;"]
+    (after [end-state ["attr name string one;"
+                       "attr name string many;"]]
       (is (not (problems? end-state)))
       (attr? :name {:db/ident       :name
                     :db/cardinality :db.cardinality/many}))))
 
 (deftest relation
   (testing "can be added"
-    (after ["attr name string one;"
-            "attr age long one;"
-            "relation person-age name age;"]
+    (after [end-state ["attr name string one;"
+                       "attr age long one;"
+                       "relation person-age name age;"]]
       (is (not (problems? end-state)))
       (relation? :person-age)))
 
   (testing "can be one attribute wide"
-    (after ["attr name string one;"
-            "relation system name;"]
+    (after [end-state ["attr name string one;"
+                       "relation system name;"]]
       (is (not (problems? end-state)))
       (relation? :system)))
 
   (testing "can have a value added"
-    (after ["attr name string one;"
-            "attr age long one;"
-            "relation person-age name age;"
-            "person-age \"rajesh\" 25;"]
-      (is (= #{["rajesh" 25]}
-            (qr :person-age "rajesh" '?age)))))
+    (after [end-state ["attr name string one;"
+                      "attr age long one;"
+                      "relation person-age name age;"
+                      "person-age \"rajesh\" 25;"]]
+           (is (= #{["rajesh" 25]}
+                  (qr :person-age "rajesh" '?age)))))
 
   (testing "can have several values added"
-    (after ["attr name string one;"
-            "attr age long one;"
-            "relation person-age name age;"
-            "person-age \"douglas\" 25;"
-            "person-age \"sarai\"   39;"
-            "person-age \"rajesh\"  25;"]
+    (after [end-state ["attr name string one;"
+                      "attr age long one;"
+                      "relation person-age name age;"
+                      "person-age \"douglas\" 25;"
+                      "person-age \"sarai\"   39;"
+                      "person-age \"rajesh\"  25;"]]
       (is (= #{["rajesh" 25] ["douglas" 25]}
             (qr :person-age '?name 25)))))
 
   (testing "each value is unique"
-    (after ["attr name string one;"
-            "attr age long one;"
-            "relation person-age name age;"
-            "person-age \"rajesh\"  25;"
-            "person-age \"rajesh\"  25;"
-            "person-age \"rajesh\"  25;"]
+    (after [end-state ["attr name string one;"
+                       "attr age long one;"
+                       "relation person-age name age;"
+                       "person-age \"rajesh\"  25;"
+                       "person-age \"rajesh\"  25;"
+                       "person-age \"rajesh\"  25;"]]
       (is (= 1
             (count (qr :person-age "rajesh" 25))))))
 
   (testing "different relations with the same attributes"
     (testing "are allowed"
-      (after ["attr name string one;"
-              "attr years long one;"
-              "relation person-age name years;"
-              "relation employment-duration name years;"]
+      (after [end-state ["attr name string one;"
+                         "attr years long one;"
+                         "relation person-age name years;"
+                         "relation employment-duration name years;"]]
         (is (not (problems? end-state)))
         (relation? :person-age)
         (relation? :employment-duration)))
 
     (testing "and their values are distinct"
-      (after ["attr name string one;"
-              "attr years long one;"
-              "relation person-age name years;"
-              "relation employment-duration name years;"
-              "person-age \"rajesh\" 25;"
-              "employment-duration \"rajesh\" 3;"]
+      (after [end-state ["attr name string one;"
+                         "attr years long one;"
+                         "relation person-age name years;"
+                         "relation employment-duration name years;"
+                         "person-age \"rajesh\" 25;"
+                         "employment-duration \"rajesh\" 3;"]]
         (is (not (problems? end-state)))
         (is (= #{["rajesh" 25]}
               (qr :person-age "rajesh" '?age)))
@@ -133,55 +129,55 @@
 
 (deftest queries
   (testing "a query looks like an instance with a logic variable"
-    (after ["attr name string one;"
-            "attr age long one;"
-            "relation person-age name age;"
-            "person-age \"rajesh\" 25;"]
+    (after [end-state ["attr name string one;"
+                       "attr age long one;"
+                       "relation person-age name age;"
+                       "person-age \"rajesh\" 25;"]]
       (is (= #{["rajesh" 25]} (:query-result (pq "person-age ?name 25;"))))))
 
   (testing "querying a single-value relation returns the set of values"
-    (after ["attr name string one;"
-            "relation person name;"
-            "person \"douglas\";"
-            "person \"sarai\";"
-            "person \"rajesh\";"]
+    (after [end-state ["attr name string one;"
+                       "relation person name;"
+                       "person \"douglas\";"
+                       "person \"sarai\";"
+                       "person \"rajesh\";"]]
       (is (= #{["rajesh"] ["sarai"] ["douglas"]} (:query-result (pq "person ?n;"))))))
 
   (testing "a query can have multiple clauses"
-    (after ["attr name string one;"
-            "attr age long one;"
-            "relation person-age name age;"
-            "person-age \"rajesh\" 25;"
-            "attr location string one;"
-            "relation assignment name location;"
-            "assignment \"rajesh\" \"southlake\";"]
+    (after [end-state ["attr name string one;"
+                       "attr age long one;"
+                       "relation person-age name age;"
+                       "person-age \"rajesh\" 25;"
+                       "attr location string one;"
+                       "relation assignment name location;"
+                       "assignment \"rajesh\" \"southlake\";"]]
       (is (= #{["rajesh" 25 "southlake"]} (:query-result (pq "person-age ?name 25, assignment ?name \"southlake\";"))))))
 
   (testing "junction"
-    (after ["attr name string one;"
-            "attr age long one;"
-            "relation person-age name age;"
-            "person-age \"douglas\" 25;"
-            "person-age \"sarai\"   39;"
-            "person-age \"rajesh\"  25;"]
+    (after [end-state ["attr name string one;"
+                       "attr age long one;"
+                       "relation person-age name age;"
+                       "person-age \"douglas\" 25;"
+                       "person-age \"sarai\"   39;"
+                       "person-age \"rajesh\"  25;"]]
       (is (=  #{["rajesh" 25]} (:query-result (pq "person-age ?name ?age, = ?name \"rajesh\";"))))))
 
   (testing "disjunction"
-    (after ["attr name string one;"
-            "attr age long one;"
-            "relation person-age name age;"
-            "person-age \"douglas\" 25;"
-            "person-age \"sarai\"   39;"
-            "person-age \"rajesh\"  25;"]
+    (after [end-state ["attr name string one;"
+                       "attr age long one;"
+                       "relation person-age name age;"
+                       "person-age \"douglas\" 25;"
+                       "person-age \"sarai\"   39;"
+                       "person-age \"rajesh\"  25;"]]
       (is (=  #{["douglas" 25]} (:query-result (pq "person-age ?name 25, != ?name \"rajesh\";"))))))
 
   (testing "inequality"
-    (after ["attr name string one;"
-            "attr age long one;"
-            "relation person-age name age;"
-            "person-age \"douglas\" 25;"
-            "person-age \"sarai\"   39;"
-            "person-age \"rajesh\"  25;"]
+    (after [end-state ["attr name string one;"
+                        "attr age long one;"
+                        "relation person-age name age;"
+                        "person-age \"douglas\" 25;"
+                        "person-age \"sarai\"   39;"
+                        "person-age \"rajesh\"  25;"]]
       (are [expected query] (= expected (:query-result (pq query)))
         #{["douglas" 25] ["rajesh" 25]} "person-age ?name ?age, < ?age 30;"
         #{["douglas" 25] ["rajesh" 25]} "person-age ?name ?age, <= ?age 25;"
@@ -189,87 +185,87 @@
         #{["sarai" 39]}                 "person-age ?name ?age, >= ?age 39;")))
 
   (testing "self-joins are allowed"
-    (after ["attr a string one;"
-            "attr b string one; "
-            "relation foo a b;"
-            "foo \"cake\" \"pie\";"
-            "foo \"cake\" \"cake\";"]
+    (after [end-state ["attr a string one;"
+                       "attr b string one; "
+                       "relation foo a b;"
+                       "foo \"cake\" \"pie\";"
+                       "foo \"cake\" \"cake\";"]]
       (is (= #{["cake"]} (:query-result (pq "foo ?a ?a;")))))))
 
 (deftest query-fields
   (testing "fields are named for the logic variables"
-    (after ["attr name string one;"
-            "relation location name;"
-            "location \"NYC\";"
-            "location \"anywhere else\";"]
+    (after [end-state ["attr name string one;"
+                       "relation location name;"
+                       "location \"NYC\";"
+                       "location \"anywhere else\";"]]
       (is (= '[?name] (:query-fields (pq "location ?name;"))))
       (is (= '[?cake] (:query-fields (pq "location ?cake;")))))
 
-    (after ["attr name string one;"
-            "relation seats name name name name;"
-            "seats \"a\" \"b\" \"c\" \"d\";"]
+    (after [end-state ["attr name string one;"
+                       "relation seats name name name name;"
+                       "seats \"a\" \"b\" \"c\" \"d\";"]]
       (is (= '[?a ?b ?c ?d] (:query-fields (pq "seats ?a ?b ?c ?d;")))))
 
-    (after ["attr name string one;"
-            "attr age long one;"
-            "relation person-age name age;"
-            "person-age \"douglas\" 25;"
-            "person-age \"sarai\"   39;"
-            "person-age \"rajesh\"  25;"
-            "relation person-title name name;"
-            "person-title \"douglas\" \"associate\";"
-            "person-title \"sarai\"   \"staff\";"
-            "person-title \"joan\"    \"host\";"]
+    (after [end-state ["attr name string one;"
+                       "attr age long one;"
+                       "relation person-age name age;"
+                       "person-age \"douglas\" 25;"
+                       "person-age \"sarai\"   39;"
+                       "person-age \"rajesh\"  25;"
+                       "relation person-title name name;"
+                       "person-title \"douglas\" \"associate\";"
+                       "person-title \"sarai\"   \"staff\";"
+                       "person-title \"joan\"    \"host\";"]]
       (is (= '[?name ?age ?title] (:query-fields (pq "person-age ?name ?age, person-title ?name ?title;")))))))
 
 (deftest fields-are-ordered-not-named
   (testing "an instance's fields are kept separate"
-    (after ["attr name string one;"
-            "relation seats name name name name;"
-            "seats \"a\" \"b\" \"c\" \"d\";"]
+    (after [end-state ["attr name string one;"
+                       "relation seats name name name name;"
+                       "seats \"a\" \"b\" \"c\" \"d\";"]]
       (is (= #{["a" "b" "c" "d"]} (:query-result (pq "seats ?a ?b ?c ?d;")))))))
 
 (deftest colon-is-shorthand-for-repetition
   (testing "in instances"
-    (after ["attr id symbol one;"
-            "relation diagram id;"
-            "diagram: sysml-diagram behavior-diagram requirements-diagram;"]
+    (after [end-state ["attr id symbol one;"
+                       "relation diagram id;"
+                       "diagram: sysml-diagram behavior-diagram requirements-diagram;"]]
       (is (=  #{['sysml-diagram] ['behavior-diagram] ['requirements-diagram]}
             (:query-result (pq "diagram ?a;")))))
 
-    (after ["attr id symbol one;"
-            "relation specialize id id;"
-            "specialize sysml-diagram: behavior-diagram requirements-diagram structural-diagram;"]
+    (after [end-state ["attr id symbol one;"
+                       "relation specialize id id;"
+                       "specialize sysml-diagram: behavior-diagram requirements-diagram structural-diagram;"]]
       (is (=  #{'[sysml-diagram behavior-diagram]
                  '[sysml-diagram requirements-diagram]
                 '[sysml-diagram structural-diagram]}
             (:query-result (pq "specialize ?par ?cld;")))))
 
-    (after ["attr id symbol one;"
-            "attr label string one;"
-            "relation diagram-kind id label label;"
-            "diagram-kind:
+    (after [end-state ["attr id symbol one;"
+                       "attr label string one;"
+                       "relation diagram-kind id label label;"
+                       "diagram-kind:
                activity-diagram         \"act\" \"Activity Diagram\"
                block-definition-diagram \"bdd\" \"Block Definition Diagram\"
                internal-block-diagram   \"ibd\" \"Internal Block Diagram\"
-               package-diagram          \"pkg\" \"Package Diagram\";"]
+               package-diagram          \"pkg\" \"Package Diagram\";"]]
       (is (= #{['activity-diagram "act" "Activity Diagram"]}
             (:query-result (pq "diagram-kind ?d ?a ?b, = ?a \"act\"; "))))
       (is (= 4 (count (:query-result (pq "diagram-kind ?kind ?tag ?label;"))))))))
 
 (deftest relations-can-constrain-values
   (testing "values in the confining relation are accepted"
-    (after ["attr name string one;"
-            "relation direction name;"
-            "direction:\"N\" \"NE\" \"E\" \"SE\" \"S\" \"SW\" \"W\" \"NW\";"
-            "relation arrow (name in direction);"]
+    (after [end-state ["attr name string one;"
+                       "relation direction name;"
+                       "direction:\"N\" \"NE\" \"E\" \"SE\" \"S\" \"SW\" \"W\" \"NW\";"
+                       "relation arrow (name in direction);"]]
       (pq "arrow \"N\";")))
 
   (testing "values not in the confining relation are rejected"
-    (after ["attr name string one;"
-            "relation direction name;"
-            "direction:\"N\" \"NE\" \"E\" \"SE\" \"S\" \"SW\" \"W\" \"NW\";"
-            "relation arrow (name in direction);"]
+    (after [end-state ["attr name string one;"
+                       "relation direction name;"
+                       "direction:\"N\" \"NE\" \"E\" \"SE\" \"S\" \"SW\" \"W\" \"NW\";"
+                       "relation arrow (name in direction);"]]
       (is (thrown? AssertionError (pq "arrow \"to the knee\";"))))))
 
 (defn- pb [input]
@@ -287,18 +283,18 @@
    :query-result (set result)})
 
 (deftest binding-expressions
-  (after ["attr id symbol one;"
-          "attr label string one;"
-          "attr name string one;"
-          "relation diagram-kind id label label;"
-          "diagram-kind:
+  (after [end-state ["attr id symbol one;"
+                     "attr label string one;"
+                     "attr name string one;"
+                     "relation diagram-kind id label label;"
+                     "diagram-kind:
                activity-diagram         \"act\" \"Activity Diagram\"
                block-definition-diagram \"bdd\" \"Block Definition Diagram\"
                internal-block-diagram   \"ibd\" \"Internal Block Diagram\"
                package-diagram          \"pkg\" \"Package Diagram\";"
-          "relation direction name;"
-          "direction:\"N\" \"E\" \"S\" \"W\";"
-          "relation arrow (name in direction);"]
+                     "relation direction name;"
+                     "direction:\"N\" \"E\" \"S\" \"W\";"
+                     "relation arrow (name in direction);"]]
 
     (are [qry m] (= m (pb qry))
       "{ };"
